601da21952f50e871ed6d59f83f7f072a425d7b2
bdjos1 <o_sullivanbarry@hotmail.com>
2022-06-22 15:35:48 +0100

    Revert previous commit

--- packages/custom-carbon-addons/src/apollo-client-hooks/index.js
+++ packages/custom-carbon-addons/src/apollo-client-hooks/index.js
@@ -8,16 +8,5 @@
  * US Government Users Restricted Rights - Use, duplication or disclosure
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  */
-/* eslint import/no-unresolved: [2, { ignore: ['devenv_pkg*.'] }] */
-import { ApolloClientHooks } from 'devenv_pkg/src/apollo-client-hooks';
-import * as CustomApolloClientHooks from './component_hooks/components';
 
-
-// ApolloClientHooks object initially has the list of sample components to be renderered
-// The custom appollo client hooks are automatically added to the sample ones below
-// INSTRUCTIONS: Add custom ones to components/index.js (and/or feature/index.js)
-
-const updatedApolloClientHooks = { ...ApolloClientHooks, ...CustomApolloClientHooks };
-
-// eslint-disable-next-line import/prefer-default-export
-export { updatedApolloClientHooks as ApolloClientHooks };
+export { };
