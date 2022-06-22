4e4846a0355b7af9240dd41f2a63b05dbbe1baf8
bdjos1 <o_sullivanbarry@hotmail.com>
2022-06-22 15:06:54 +0100

    Updating Logo applo client hook sample

--- packages/carbon-addons-devenv/src/apollo-client-hooks/hook_samples/Logo/LogoQuery.js
+++ packages/carbon-addons-devenv/src/apollo-client-hooks/hook_samples/Logo/LogoQuery.js
@@ -3,16 +3,15 @@
  *
  * PID 5725-H26
  *
- * Copyright IBM Corporation 2020. All Rights Reserved.
+ * Copyright IBM Corporation 2021. All Rights Reserved.
  *
  * US Government Users Restricted Rights - Use, duplication or disclosure
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  */
-
 import { gql, useQuery } from '@apollo/client';
 
 const GetLogo = gql`
-  query GetLogo {
+  query GetLogo($logoId: String!)  {
     logo(logoId: $logoId) {
       name
     }
--- packages/carbon-addons-devenv/src/apollo-client-hooks/hook_samples/Logo/LogoQueryProvider.js
+++ packages/carbon-addons-devenv/src/apollo-client-hooks/hook_samples/Logo/LogoQueryProvider.js
@@ -3,7 +3,7 @@
  *
  * PID 5725-H26
  *
- * Copyright IBM Corporation 2020. All Rights Reserved.
+ * Copyright IBM Corporation 2021.2022. All Rights Reserved.
  *
  * US Government Users Restricted Rights - Use, duplication or disclosure
  * restricted by GSA ADP Schedule Contract with IBM Corp.
@@ -11,16 +11,22 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import Logo  from '../../../react/component_samples/Logo/Logo';
-import ApolloClientQueryProvider from '../../core/ApolloClientQueryProvider';
+/* eslint import/no-unresolved: [2, { ignore: ['devenv_pkg*.'] }] */
+import Logo  from 'devenv_pkg/src/react/component_samples/Logo/Logo';
+// NOTE: 
+// from custom-carbon-addons package reference to ApolloClientQueryProvider would be he following
+// import { ApolloClientQueryProvider } from 'devenv_pkg/src/apollo-client-hooks';
+import ApolloClientQueryProvider from 'devenv_pkg/src/apollo-client-hooks/core/ApolloClientQueryProvider';
 import { useGetLogo } from './LogoQuery';
 
 const LogoQueryProvider = ({ configuration }) => {
 
-  const { data } = useGetLogo();
-  const logo = (<>{data && data.logo.map(logoItem => (
-    <Logo key={logoItem.logoId} imageName={logoItem.name}/>
-   ))}</>);
+  const { data } = useGetLogo(configuration.logoId);
+  const propertyName = 'logo';
+  // dataa in this instance is juts an oject rather than an array
+  const logo = (<>{data && data[propertyName] && (
+    <Logo key={data[propertyName].logoId} imageName={data[propertyName].name}/>
+   )}</>);
 
   if (!data) return <></>;
 
