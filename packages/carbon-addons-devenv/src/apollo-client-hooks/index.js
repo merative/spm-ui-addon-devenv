cbc1b7a628343c499b206bb2a8881a5703d92fd1
bdjos1 <o_sullivanbarry@hotmail.com>
2022-06-22 16:14:17 +0100

    Updated name and refactoring
    Refactoring children around devenv_pkg import and changing name to more intuitive one

--- packages/carbon-addons-devenv/src/apollo-client-hooks/index.js
+++ packages/carbon-addons-devenv/src/apollo-client-hooks/index.js
@@ -11,15 +11,22 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PeopleDataProvider from './hook_samples/Person';
-import LogoDataProvider from './hook_samples/Logo';
+import PersonComponentApolloClientHook from './hook_samples/Person';
+import LogoComponentApolloClientHook from './hook_samples/Logo';
 import ApolloClientConfig from './core/ApolloClientConfig';
 import ApolloClientDataProvider from './core/ApolloClientDataProvider';
 import ApolloClientQueryProvider from './core/ApolloClientQueryProvider';
 import Renderer from '../../sample_renderers/Renderer';
 import settings from '../react';
 
-const RenderWithApolloClient = (container, configuration, DataProvider) => {
+/**
+ * Contains a list of all the custom appolo client hooks with components
+ * and features that can be rendered in the application usrt interface.
+ * @param {*} container The element.
+ * @param {*} configuration The apollo client configuration
+ * @param {*} DataProvider The apollo data provider
+ */
+const ApolloClientHookTemplate = (container, configuration, DataProvider) => {
   const apolloClient = ApolloClientConfig.getConfig(configuration);
   
   ReactDOM.render(
@@ -31,16 +38,9 @@ const RenderWithApolloClient = (container, configuration, DataProvider) => {
   );
 };
 
-const RenderLogoWithApolloClient = (container, configuration) => {
-  RenderWithApolloClient(container, configuration, LogoDataProvider);
-};
+// sample apollo client compponents used
+const SampleApolloClientHooks = { LogoComponentApolloClientHook, PersonComponentApolloClientHook };
 
-
-const RenderPersonWithApolloClient = (container, configuration) => {
-  RenderWithApolloClient(container, configuration, PeopleDataProvider);
-};
-
-// this is the the 2 sample apollo client compponents used
-const RendererListWithApolloClient = { RenderLogoWithApolloClient, RenderPersonWithApolloClient };
-
-export { RendererListWithApolloClient, RenderWithApolloClient, ApolloClientDataProvider, ApolloClientQueryProvider, Renderer, settings };
+// ApolloClientHookTemplate,  ApolloClientDataProvider, ApolloClientQueryProvider, Renderer are all apollo client helper objects that
+// need to be referened in custom-carbon-addons package
+export { SampleApolloClientHooks as ApolloClientHooks, ApolloClientHookTemplate, ApolloClientDataProvider, ApolloClientQueryProvider, Renderer, settings };
