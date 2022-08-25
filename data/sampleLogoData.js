65b9588c817bb7cfdb18acbc748ace16534791fd
bdjos1 <o_sullivanbarry@hotmail.com>
2022-08-25 23:09:00 +0100

    Delete sampleLogoData.js

--- data/sampleLogoData.js
+++
@@ -1,9 +0,0 @@
-
-const logoData = (parent, args) => {
-  if (args.logoId == 'id_1') {
-    return {name: 'avatar__adult--125-enabled.svg', logoId: 'id_1'};
-  }
-  return {name: 'avatar__child--125-enabled.svg', logoId: 'id_2'};
-}
-  
-module.exports = { logoData };
