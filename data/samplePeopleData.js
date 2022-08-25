d7fbacffef4b416f2027a3787f190512d82e9810
bdjos1 <o_sullivanbarry@hotmail.com>
2022-08-25 23:09:15 +0100

    Delete samplePeopleData.js

--- data/samplePeopleData.js
+++
@@ -1,5 +0,0 @@
-const personsData = () => {
-  return [{id: 'id_1', firstname: 'John BAM', surname: 'Senior', age: 70}, {id: 'id_2', firstname: 'Jennifer BAM', surname: 'Middleage', age: 42}, {id: 'id_3', firstname: 'Sam BAM', surname: 'Young', age: 12}];
-}
-
-module.exports = { personsData };
