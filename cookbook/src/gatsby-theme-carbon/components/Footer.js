48b968272e3671aa0ef1f1d7804aeea9f82a13ce
Jenny Cooper <jcooper3@ie.ibm.com>
2023-02-09 19:03:01 +0000

    Update Footer.js

--- cookbook/src/gatsby-theme-carbon/components/Footer.js
+++ cookbook/src/gatsby-theme-carbon/components/Footer.js
@@ -103,7 +103,7 @@ const DefaultContent = ({ buildTime }) => (
     <p>
       Docs last updated: {buildTime}
       <br />
-      &copy; Merative US L.P. 2021, 2022
+      &copy; Merative US L.P. 2021, 2023
       <br />
       Merative is a trademark of Merative US L.P. in the United States and other countries.
     </p>
