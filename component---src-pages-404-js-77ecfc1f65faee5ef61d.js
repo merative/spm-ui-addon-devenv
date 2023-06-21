b7fd1801db5c0fa88d7d034b266499a974a612a8
ajc24 <ajc24@users.noreply.github.com>
2023-06-21 15:36:58 +0000

    Deploy commit: 0c630e04a9edb5124b2bef185757a1a5adedd35c

---
+++ component---src-pages-404-js-77ecfc1f65faee5ef61d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkexample=self.webpackChunkexample||[]).push([[883],{429:function(e,n,t){t.r(n);var o=t(7294),r=t(8680),s=[{href:"/components/markdown",text:"Markdown"},{href:"/components/Aside",text:"Aside"},{href:"/components/demo",text:"Demo"}];n.default=function(){return o.createElement(r.e9,{links:s})}}}]);
+//# sourceMappingURL=component---src-pages-404-js-77ecfc1f65faee5ef61d.js.map
