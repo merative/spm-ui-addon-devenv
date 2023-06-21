fef3f8dd6129b6d1bf9c7e0d05e430d5c6761bf1
Anthony Tonge <tongant@merative.com>
2023-06-21 10:30:54 +0100

    redwash copyrights

--- packages/carbon-addons-devenv/sample_renderers/logo/LogoRenderer.js
+++ packages/carbon-addons-devenv/sample_renderers/logo/LogoRenderer.js
@@ -1,14 +1,7 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 /*
- * Licensed Materials - Property of IBM
- *
- * PID 5725-H26
- *
- * Copyright IBM Corporation 2022. All Rights Reserved.
- *
- * US Government Users Restricted Rights - Use, duplication or disclosure
- * restricted by GSA ADP Schedule Contract with IBM Corp.
+ * Copyright Merative US L.P. 2022
  */
 /* eslint import/no-unresolved: [2, { ignore: ['devenv_pkg*.'] }] */
 // NOTE: from custom-carbon-addons package reference to Renderer would be he following
