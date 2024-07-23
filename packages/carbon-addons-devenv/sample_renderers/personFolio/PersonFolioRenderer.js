7df564a0f84a255b84c8c38572eb385fdf4bb0c1
bdjos1 <o_sullivanbarry@hotmail.com>
2024-07-23 19:16:53 +0100

    Remove obsolete comment from PersonFolioRenderer.js

--- packages/carbon-addons-devenv/sample_renderers/personFolio/PersonFolioRenderer.js
+++ packages/carbon-addons-devenv/sample_renderers/personFolio/PersonFolioRenderer.js
@@ -1,9 +1,7 @@
 /*
  * Copyright Merative US L.P. 2021, 2024
  */
-// NOTE:
-// If you want to use Renderer in the custom-carbon-addons package use the following import:
-// import { Renderer } from 'devenv_pkg/src/apollo-client-hooks';
+
 // eslint-disable-next-line importPlugin/no-unresolved
 import { Renderer } from "devenv_pkg/src/apollo-client-hooks";
 
